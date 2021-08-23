931d4e6f72668b6a1db0649ab1a042a198989c03
Andrew Martinez <apetermz@gmail.com>
2021-08-23 01:43:53 -0700

    Redeploy v49

--- static/js/thankyou.js
+++ static/js/thankyou.js
@@ -1,6 +1,5 @@
 document.getElementById("myForm").onsubmit = function() {myFunction()};
 
 function myFunction() {
-  alert("The form was submitted");
-  window.location.replace("http://www.w3schools.com/%22");
+  alert("Thank you. The form was submitted");
 }
