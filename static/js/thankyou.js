b8dd442168cc55aab354085a8a8daf0f29ad7c84
Andrew Martinez <apetermz@gmail.com>
2021-08-23 01:14:03 -0700

    Redeploy v46

--- static/js/thankyou.js
+++ static/js/thankyou.js
@@ -1,6 +1,5 @@
-var form = document.querySelector('.pageclip-form')
-Pageclip.form(form, {
-  onSubmit: function (event) { },
-  onResponse: function (error, response) { },
-  successTemplate: '<span>Thank you!</span>'
-})
+document.getElementById("myForm").onsubmit = function() {myFunction()};
+
+function myFunction() {
+  alert("The form was submitted");
+}
